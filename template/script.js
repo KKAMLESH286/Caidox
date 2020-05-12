510a239bacef9a4062ac74f2b49893327144515b
Kamlesh Panwar <kkamlesh286@gmail.com>
2020-05-13 00:31:05 +0530

    third commit

--- template/script.js
+++
@@ -1,9 +0,0 @@
-today=new Date();
-var cmas=new Date(today.getFullYear(), 11, 25);
-if (today.getMonth()==11 && today.getDate()>25) 
-{
-cmas.setFullYear(cmas.getFullYear()+1); 
-}  
-var one_day=1000*60*60*24;
-var line = document.write(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
-" days left");
