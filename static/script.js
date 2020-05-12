6153d24b96443a3d30de48f897c39bf5d594e1f4
Kamlesh Panwar <42006848+KKAMLESH286@users.noreply.github.com>
2020-05-13 00:49:36 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,9 +5,5 @@ if (today.getMonth()==11 && today.getDate()>25)
 cmas.setFullYear(cmas.getFullYear()+1); 
 }  
 var one_day=1000*60*60*24;
-<<<<<<< HEAD:static/script.js
 document.write(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
 " days left");
-=======
-var line = document.write(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+ " days left");
->>>>>>> 766ec883fe2f8e61dfe11b7a8bbe475b61e60f76:template/script.js
